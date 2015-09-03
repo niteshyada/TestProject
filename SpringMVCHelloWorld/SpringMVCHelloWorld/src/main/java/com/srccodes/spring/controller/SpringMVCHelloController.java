5c7cb787e4a9d8c73531c1b6133623b3e9d18fe6
Nitesh Yadav <niteshyada@gmail.com>
2015-09-03 16:30:25 +0530

    added java doc

--- SpringMVCHelloWorld/SpringMVCHelloWorld/src/main/java/com/srccodes/spring/controller/SpringMVCHelloController.java
+++ SpringMVCHelloWorld/SpringMVCHelloWorld/src/main/java/com/srccodes/spring/controller/SpringMVCHelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
- * @author Abhijit Ghosh
+ * @author Nitesh Yadav
  * @version 1.0
  */
 
